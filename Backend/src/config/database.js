141b55bc914266d50d6a31c0ac753aab39daad21
unknown <gabrielchagasboff@gmail.com>
2022-08-23 20:48:28 -0300

    Database Configuration

---
+++ Backend/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "ProductManager",
+    password: "Product12345",
+    database: "stock_product",
+    define: {
+        timestarps: true,
+        underscored: true,
+        
+    },
+};
---
+++ Backend/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+app.use(routes);
+app.listen(port);
